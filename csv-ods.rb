bd1a3c1132bee1868ed662d75a3ac7332c2361ef
Enrique Orestes Santos, Jr <me@enriquesantos.net>
2013-01-10 23:57:30 -0800

    now writes CSVs to XLSX workbook

--- csv-ods.rb
+++ csv-ods.rb
@@ -7,6 +7,7 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'axlsx'
+require 'csv'
 
 p = Axlsx::Package.new
 p.use_shared_strings = true
@@ -16,12 +17,14 @@ out_file = ARGV[1]
 
   Dir.glob(dir + '/*.csv') do |csvf|
     table_name = csvf.split(/\/|[.]/)[-2]
-
-     p.workbook do |wb|
-       wb.add_worksheet(:name => table_name) do |ws|
-           # ws.add_row # array csv by row
-       end
-     end
+    puts table_name
+    p.workbook do |wb|
+      wb.add_worksheet(:name => table_name) do |ws|
+        CSV.foreach(csvf) do |row|
+	  ws.add_row row
+	end
+      end
+    end
   end
 
 p.serialize out_file
